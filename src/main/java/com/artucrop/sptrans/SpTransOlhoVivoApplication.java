a132d6179f8bc2f38d2139bb2e9cc7e07a441711
abmaschietto <arturboudoux@gmail.com>
2023-04-27 21:01:10 -0300

    Initial commit

---
+++ src/main/java/com/artucrop/sptrans/SpTransOlhoVivoApplication.java
@@ -0,0 +1,13 @@
+package com.artucrop.sptrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpTransOlhoVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpTransOlhoVivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artucrop/sptrans/SpTransOlhoVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artucrop.sptrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpTransOlhoVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
